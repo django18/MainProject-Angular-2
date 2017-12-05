a942d6b1755b835a1b4ebae83f98cda9f8e9e949
Amit Dangi <avinashdangi.26@gmail.com>
2017-12-05 21:25:05 +0530

    Component Generation for main project

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredients } from './ingredients.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients:Ingredients[]=[
+    new Ingredients('Onion',5),
+    new Ingredients('Tomato',7)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
