4d6587ba2627a8ce6780b0b385356c23b266bfba
Amit Dangi <avinashdangi.26@gmail.com>
2017-12-29 23:43:34 +0530

    Forms integrated for editing shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,11 @@
 import { Ingredients } from "./ingredients.model";
 import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs/Subject";
 
 
 export class ShoppingListService{
     
-    ingredientsChanged=new EventEmitter<Ingredients[]>();
+    ingredientsChanged=new Subject<Ingredients[]>();
     private ingredients:Ingredients[]=[
         new Ingredients('Onion',5),
         new Ingredients('Tomato',7)
@@ -19,11 +20,11 @@ export class ShoppingListService{
   
     add(ingredient:Ingredients){
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients:Ingredients[]){
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
